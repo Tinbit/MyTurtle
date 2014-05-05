96f3bfea514c3eb83a48d30a454875dcff6acee6
Nik Torfs <niktorfs@gmail.com>
2014-05-05 20:22:42 +0200

    Removed shadow image and redid it with css (like it was before)

--- src/js/core/screen.js
+++ src/js/core/screen.js
@@ -47,9 +47,6 @@ window.Screen = (function() {
         }
         log.info(" Done adding panes");
 
-        // add a shadow div
-        $("#container").prepend('<div id="shadow-mid"></div>');
-
 
         // create turtles
         log.info(" Start creating turtles");
